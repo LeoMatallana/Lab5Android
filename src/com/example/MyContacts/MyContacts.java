fc52177a74c8a0b3956643ebf6e7fc99139248a4
Leonardo Matallana <Zeugmax>
2015-06-24 02:21:15 -0500

    - Lab5 init

---
+++ src/com/example/MyContacts/MyContacts.java
@@ -0,0 +1,30 @@
+package com.example.MyContacts;
+
+import android.app.Application;
+import com.example.MyContacts.data.Contact;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Zeugmax on 24/06/2015.
+ */
+public class MyContacts extends Application
+{
+    public ArrayList<Contact> myContacts;
+
+    public MyContacts() { myContacts = new ArrayList<Contact>();}
+
+    public Contact searchContactByName(String name)
+    {
+        Contact myContact = null;
+        for(Contact contact : myContacts)
+        {
+            if(contact.getName()==name)
+            {
+                myContact = contact;
+                break;
+            }
+        }
+        return myContact;
+    }
+}
